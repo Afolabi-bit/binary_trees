29e3072491aac02f6a298ad523ee633b5df2939f
afolabi <maverickoluwatomisin@gmail.com>
2023-02-28 21:35:15 +0100

    create

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,15 +5,18 @@
  * @parent: a pointer to the parent node of the node to create
  * @value: is the value to put in the new node
 */
-*binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *temp = (binary_tree_t) malloc(sizeof(binary_tree_t));
+    binary_tree_t *temp = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+    if (temp == NULL)
+    {
+        return (NULL);
+    }
+    temp->parent = parent;
     temp->n = value;
     temp->left = NULL;
     temp->right = NULL;
 
-    if (parent == NULL)
-    {
-        parent = temp;
-    }
+
+    return (temp);
 }
