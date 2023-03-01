771cf8087cce409b938b2d94b3fdc6b1a0b53c7e
afolabi <maverickoluwatomisin@gmail.com>
2023-03-01 21:20:51 +0100

    insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,8 +15,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
     }
     temp->parent = parent;
     temp->n = value;
-    temp->left = NULL;
+    temp->left = parent->left;
     temp->right = NULL;
     parent->left = temp;
-    return (parent->left);
+
+    if (temp->left)
+    {
+        temp->left->parent = temp;
+    }
+    return (temp);
 }
