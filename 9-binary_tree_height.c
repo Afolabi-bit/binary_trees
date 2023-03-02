de62af7841f6c2e7014047067314b74697f70be9
afolabi <maverickoluwatomisin@gmail.com>
2023-03-02 21:34:51 +0100

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,11 +8,11 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-if (tree == NULL)
-return (0);
-
 size_t leftHeight = 0;
 size_t rightHeight = 0;
+
+if (tree == NULL)
+return (0);
 
 if (tree->left)
 {
