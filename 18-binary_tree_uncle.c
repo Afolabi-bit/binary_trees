0f94c294b1f5c0e959eaa67cc4646c58e846f939
afolabi <maverickoluwatomisin@gmail.com>
2023-03-03 18:08:32 +0100

    uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,7 +9,7 @@
 */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-if (!node || node->parent->parent == NULL)
+if (!node || !node->parent || !node->parent->parent)
 return (NULL);
 
 if (node->parent == node->parent->parent->left)
