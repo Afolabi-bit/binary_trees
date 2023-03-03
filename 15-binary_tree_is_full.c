69acbce46d7e55b8748c2a5042e5f7bbcfc3eff4
afolabi <maverickoluwatomisin@gmail.com>
2023-03-03 15:49:53 +0100

    full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,12 +10,12 @@ int binary_tree_is_full(const binary_tree_t *tree)
 if (tree == NULL)
 return (0);
 
-if ((tree->left && tree->right) || (!tree->left && tree->right))
-binary_tree_is_full(tree->left);
-binary_tree_is_full(tree->right);
+if (!tree->left && !tree->right)
 return (1);
-}
-else
-{
+
+if (tree->left && tree->right)
+return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+
 return (0);
+
 }
