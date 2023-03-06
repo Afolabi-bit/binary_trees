2ad685ce9ca8116528754dd0a8f4b37501eedb16
afolabi <maverickoluwatomisin@gmail.com>
2023-03-07 00:33:29 +0100

    levelorder

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -36,7 +36,8 @@ return (leftHeight > rightHeight ? leftHeight : rightHeight);
 
 
 /**
- * binary_tree_levelorder - goes through a binary tree using level-order traversal
+ * binary_tree_levelorder - goes through a binary
+ * tree using level-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
  * Return: void
