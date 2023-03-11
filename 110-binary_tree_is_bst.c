49ea5910a4adbd03d5702fbc43bc3a7784e4eb02
afolabi <maverickoluwatomisin@gmail.com>
2023-03-11 22:23:18 +0100

    isBst

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -10,11 +10,12 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 if (tree == NULL)
 return (1);
 
-if (tree->left != NULL && root->left->n > tree->n)
+if (tree->left != NULL && tree->left->n > tree->n)
 return (0);
-}
+
 
 if (tree->right != NULL && tree->right->n <= tree->n)
 return (0);
 
-return (binary_tree_is_bst(tree->left) && binary_tree_is_bst(tree->right))
+return (binary_tree_is_bst(tree->left) && binary_tree_is_bst(tree->right));
+}
